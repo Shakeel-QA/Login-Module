3ec1e19a9660e56df8dc93c89be7e633f32adc03
Shakeel <Shakeelsiddique345@gmail.com>
2023-01-09 17:35:23 +0500

    Test case updated

--- src/Main Page.py
+++ src/Main Page.py
@@ -20,8 +20,11 @@ def make_csv(filename: str, data, new=True):
     with open(filename, mode, newline='') as f:
         f.writelines(data)
 
-USER_NAME = "Shakeel_QA_Admin"
-PASS_WORD = "AA@@1122"
+CORRECT_USERNAME = "Shakeel_QA_Admin"
+COORECT_PASSWORD = "AA@@1122"
+
+WRONG_USERNAME = "QA_Automation_User"
+WRONG_PSWD = "harry123"
 
 
 
@@ -29,39 +32,40 @@ def main():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.maximize_window()
     login = LoginPage(driver)
-    login.enter_username(LoginModuleResources.username, 'Shakeel_QA_Admin')
-    login.enter_password(LoginModuleResources.password, 'PAA@@##44')
+    login.enter_username(LoginModuleResources.username, CORRECT_USERNAME)
+    login.enter_password(LoginModuleResources.password, WRONG_PSWD)
     url = (driver.current_url)
     login.submit_btn(LoginModuleResources.submit_button)
     time.sleep(0.5)
     today = date.today()
     pop = driver.find_element(By.ID,"popUpMessage").text  
     make_csv('Login Report.csv',f'Test Case,Scenario, Result{today}, URL,Fail/Pass\n', new=True)
+    make_csv('Login Report.csv',f'Login Credential,Username:{CORRECT_USERNAME},Password:{COORECT_PASSWORD},\n', new=False)
     make_csv('Login Report.csv',f'Login Module,Login With Correct Username and Wrong Password,{pop},{url}\n', new=False)
     time.sleep(3)
     
     login1 = LoginPage(driver)
-    login1.enter_username(LoginModuleResources.username, 'Shakeel_QA_Admin')
-    login1.enter_password(LoginModuleResources.password, 'AA@@1122324')
+    login1.enter_username(LoginModuleResources.username, WRONG_USERNAME)
+    login1.enter_password(LoginModuleResources.password, COORECT_PASSWORD)
     login.submit_btn(LoginModuleResources.submit_button)
     time.sleep(0.5)
     pop1 = driver.find_element(By.ID,"popUpMessage").text  
-    make_csv('Login Report.csv',f'Login Module,Login With Wrong Username and Wrong Password,{pop1},{url}\n', new=False)
+    make_csv('Login Report.csv',f'Login Module,Login With Wrong Username and Correct Password,{pop1},{url}\n', new=False)
     time.sleep(3)
    
     login2 = LoginPage(driver)
-    login2.enter_username(LoginModuleResources.username, USER_NAME)
-    login2.enter_password(LoginModuleResources.password, PASS_WORD)
+    login2.enter_username(LoginModuleResources.username, CORRECT_USERNAME)
+    login2.enter_password(LoginModuleResources.password, COORECT_PASSWORD)
     url = (driver.current_url)
     login.submit_btn(LoginModuleResources.submit_button)
     url_s = (driver.current_url)                                                           
     time.sleep(0.5)
     
-    make_csv('Login Report.csv',f'Login Module,Login With Correct Username and Password,Login Successfully,{url_s}\n', new=False)
+    make_csv('Login Report.csv',f'Login Module,Login With Correct Username and Correct Password,Login Successfully,{url_s}\n', new=False)
     time.sleep(3)
 
 
-    make_csv('Login Report.csv',f'Login Credential,Username:{USER_NAME},Password:{PASS_WORD},\n', new=False)
+    
 
 if __name__ == '__main__':
     main()
